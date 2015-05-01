dc6425e836ee7decddf4ca964ff353091f351699
Max Davidson <davidsonmax@gmail.com>
2015-05-01 13:48:57 +0200

    Added Gulp tasks for semver bumps and tags

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+var fs = require('fs');
+
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+
 
 gulp.task('build', function () {
     return gulp.src('src/yadi.js')
@@ -14,4 +19,26 @@ gulp.task('build', function () {
         .pipe(uglify())
         .pipe(rename('yadi.min.js'))
         .pipe(gulp.dest('dist'));
+});
+
+function inc(type) {
+    return function () {
+        return gulp.src('./package.json')
+            .pipe(bump({ type: type }))
+            .pipe(gulp.dest('./'));
+    };
+}
+
+gulp.task('bump:patch', inc('patch'));
+gulp.task('bump:feature', inc('minor'));
+gulp.task('bump:release', inc('major'));
+
+gulp.task('tag', ['build'], function () {
+    var pkg = JSON.parse(fs.readFileSync('./package.json'));
+    var version = 'v' + pkg.version;
+
+    return gulp.src(['./dist/*', './package.json'])
+        .pipe(git.add())
+        .pipe(git.commit(version))
+        .pipe(git.tag(version, version));
 });
